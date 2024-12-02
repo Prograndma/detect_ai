b18b7df7229f4f2b525ee67b00530eda06fdb4dd
User <thomasvcole@gmail.com>
2024-12-01 22:22:07 -0700

    Adding in a function that actually WORKS. Can't believe I missed the major flaw in logic where I wasn't passing the substrings into the model at each step. Doesn't matter though, it was a slow and bad idea anyway.

--- detect.py
+++ detect.py
@@ -36,7 +36,7 @@ def get_all_sub_strs(str):
   words = str.split(" ")
   sub_strs = []
 
-  for i in range(len(words)):
+  for i in range(1, len(words)):
     sub_strs.append( (make_string(words[:i]), words[i]) )
   return sub_strs
 
@@ -44,23 +44,55 @@ def get_probs_of_seq(seq):
   prob_results = []
   with torch.no_grad():
     for (sub_seq, next_word) in get_all_sub_strs(seq):
-      inputs = tokenizer(seq, return_tensors="pt").to(device)
-      word = tokenizer(next_word, return_tensors="pt").to(device)
-
+      inputs = tokenizer(sub_seq, return_tensors="pt").to(device)
       logits = model(**inputs).logits[:, -1, :]
       probs = torch.softmax(logits, dim=-1)
       position_in_probs_of_next_word = tokenizer(next_word, return_tensors="pt").to(device)['input_ids'][0][0]
-      prob_of_next_word = probs[0][position_in_probs_of_next_word]
-      prob_results.append((prob_of_next_word, next_word))
+      prob_of_next_word = probs[0][position_in_probs_of_next_word].item()
+      prob_results.append((prob_of_next_word * 100, next_word))
   return prob_results
+
+import math
+
+def performant_prob_per_token(seq, tokenizer, model, device="cuda"):
+    prob_results = []
+    with torch.no_grad():
+        if len(seq) > 400 * 1.3:
+            seq = seq[:int(400 * 1.3)]
+        inputs = tokenizer(seq, return_tensors="pt").to(device)
+        logits = model(**inputs).logits
+        # input_ids = inputs.input_ids
+        # seq_len = input_ids.shape[1]
+        # causal_mask = torch.tril(torch.ones((seq_len, seq_len))).unsqueeze(0).unsqueeze(1)
+        # logits = model(input_ids, attention_mask=causal_mask).logits
+    probs = torch.softmax(logits, dim=-1)
+    token_ids = [inputs["input_ids"][0][i].item() for i in range(inputs["input_ids"].shape[1])]
+    token_likelihoods = probs[0, torch.arange(len(token_ids)), token_ids].tolist()
+    for i, token_likelihood in enumerate(token_likelihoods):
+        prob_results.append((token_likelihood * 100000, tokenizer.decode(token_ids[i])))
+    if len(token_ids) == 0:
+        return 0
+    return prob_results
+
 
 probs_generated = get_probs_of_seq(seq)
 probs_human = get_probs_of_seq(second_seq)
+
+
+
+print(probs_generated)
+print(probs_human)
+
+
+probs_generated = performant_prob_per_token(seq, tokenizer, model)
+probs_human = performant_prob_per_token(second_seq, tokenizer, model)
+print(probs_generated)
+print(probs_human)
+exit()
 get_all_sub_strs(seq)
 inputs = tokenizer(seq, return_tensors="pt").to(device)
 print("\nTokenized input data structure: ")
 print(inputs)
-
 word = tokenizer("researchers", return_tensors="pt").to(device)
 print(f'token for word researchers: {word["input_ids"][0][0]}')
 input_ids = inputs["input_ids"]  # just IDS, no attn mask
@@ -79,7 +111,6 @@ print(logits.shape)
 probs = torch.softmax(logits, dim=-1)
 print("\nAll probabilities: ")
 print(probs)
-
 pred_id = torch.argmax(logits).item()
 pred_word = tokenizer.decode(pred_id)
 pd.DataFrame([pred_id, logits[0, pred_id].cpu(), probs[0, pred_id].cpu(), pred_word],
@@ -161,18 +192,4 @@ greedy_output = model.generate(input_ids, max_length=50)
 
 logp = sequence_logprob(model, greedy_output, input_len=len(input_ids[0]))
 print(tokenizer.decode(greedy_output[0]))
-print(f"\nlog-prob: {logp:.2f}")
-# set seed to reproduce results. Feel free to change the seed though to get different results
-torch.random.manual_seed(42)
-
-# activate sampling and deactivate top_k by setting top_k sampling to 0
-sample_output = model.generate(
-    input_ids,
-    do_sample=True,
-    max_length=50,
-    top_k=0
-)
-
-logp = sequence_logprob(model, sample_output, input_len=len(input_ids[0]))
-print(tokenizer.decode(sample_output[0]))
 print(f"\nlog-prob: {logp:.2f}")
